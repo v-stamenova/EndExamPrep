8bd025714bf89ff17169b0b8c0e9ece1f8235b50
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-02-09 21:51:36 +0200

    Solve problem 1

---
+++ Problem-1-Pool/Pool.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Problem_1_Pool
+{
+	class Pool
+	{
+		private double radius;
+		private double height;
+
+		public Pool(double radiusInCm, double heightInCm)
+		{
+			this.radius = radiusInCm;
+			this.height = heightInCm;
+		}
+
+		public double Radius
+		{
+			get
+			{
+				return this.radius;
+			}
+			set
+			{
+				if(value > 0.0)
+				{
+					this.radius = value;
+				}
+				else
+				{
+					throw new ArgumentException("The radius cannot be less or equal to zero");
+				}
+			}
+		}
+
+		public double Height
+		{
+			get
+			{
+				return this.height;
+			}
+			set
+			{
+				if(value > 0.0)
+				{
+					this.height = value;
+				}
+				else
+				{
+					throw new ArgumentException("The radius cannot be less or equal to zero");
+				}
+			}
+		}
+
+		public double Volume()
+		{
+			return (Math.PI * Math.Pow(this.radius, 2) * this.height) / 1000000;
+		}
+
+		public double FillPrice(double pricePerCube, int tax)
+		{
+			return Volume() * pricePerCube * (1 + tax / 100);
+		}
+	}
+}
---
+++ Problem-1-Pool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Problem_1_Pool
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pool pool = new Pool(100, 50);
+
+			Console.WriteLine($"Volume of pool: {Math.Round(pool.Volume(), 2)} cm3");
+			Console.WriteLine($"Price for filling: {Math.Round(pool.FillPrice(0.90, 20), 2)} лв.");
+		}
+	}
+}
