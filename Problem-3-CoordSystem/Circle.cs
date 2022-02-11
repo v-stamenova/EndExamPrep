a79b842628e6283b47cb9c52e770ec340138a926
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-02-11 12:04:45 +0200

    Finish Problem-3

---
+++ Problem-3-CoordSystem/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_3_CoordSystem
+{
+	class Circle : RelativeCoordSystem
+	{
+		private int radius;
+
+		public Circle(int x0, int y0, int radius) : base(x0, y0)
+		{
+			this.radius = radius;
+		}
+
+		public int Radius
+		{
+			get
+			{
+				return this.radius;
+			}
+			set
+			{
+				this.radius = value;
+			}
+		}
+	}
+}
---
+++ Problem-3-CoordSystem/RelativeCoordSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_3_CoordSystem
+{
+	class RelativeCoordSystem
+	{
+		private int x0;
+		private int y0;
+
+		public RelativeCoordSystem(int x0, int y0)
+		{
+			this.x0 = x0;
+			this.y0 = y0;
+		}
+
+		public int X0
+		{
+			get
+			{
+				return this.x0;
+			}
+			set
+			{
+				this.x0 = value;
+			}
+		}
+
+		public int Y0
+		{
+			get
+			{
+				return this.y0;
+			}
+			set
+			{
+				this.y0 = value;
+			}
+		}
+	}
+}
