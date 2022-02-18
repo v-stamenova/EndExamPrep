72e65bdf36be3daa210e0a868f0ea8e56d508478
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-02-18 21:53:39 +0200

    Add solution to problem 7

---
+++ Problem-7-Hospital/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Problem_7_Hospital
+{
+	public partial class Form1 : Form
+	{
+		List<Ward> wards = new List<Ward>();
+
+		public Form1()
+		{
+			InitializeComponent();
+			DisplayAllWards();
+		}
+
+		#region Подточка А
+
+		public void DisplayAllWards() => wards.ForEach(x => richTextBox1.Text += wards.ToString() + Environment.NewLine);
+
+		#endregion
+
+		#region Подточка Б
+		private void comboBox1_DropDown(object sender, EventArgs e) => wards.Select(x => x.City).Distinct().ToList().ForEach(x => comboBox1.Items.Add(x));
+
+		private void comboBox2_DropDown(object sender, EventArgs e) => wards.Where(x => x.City == comboBox1.SelectedItem.ToString()).Select(x => x.Name).ToList().ForEach(x => comboBox2.Items.Add(x));
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			List<Ward> filteredWards = wards.Where(x => x.Name == comboBox2.SelectedItem.ToString()).ToList();
+
+			filteredWards.ForEach(x => richTextBox2.Text += $"{x.Name} - легла: {x.BedsCount}");
+		}
+
+		#endregion
+
+		#region Подточка В
+
+		private void comboBox3_DropDown(object sender, EventArgs e)
+		{
+			List<string> distinctWardNames = wards.Select(x => x.WardName).Distinct().ToList();
+			distinctWardNames.ForEach(x => comboBox3.Items.Add(x));
+		}
+
+		private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			List<Ward> filteredWards = wards.Where(x => x.WardName == comboBox3.SelectedItem.ToString()).ToList();
+
+			filteredWards.ForEach(x => richTextBox3.Text += $"Град: {x.City}; Лекари: {x.DoctorsCount}{Environment.NewLine}");
+			richTextBox3.Text += $"Лекари общо: {filteredWards.Sum(x => x.DoctorsCount)}";
+		}
+
+		#endregion
+	}
+}
---
+++ Problem-7-Hospital/Ward.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Problem_7_Hospital
+{
+	class Ward : Hospital
+	{
+		private string wardName;
+		private int bedsCount;
+		private int doctorsCount;
+
+		public Ward(string wardName, int bedsCount, int doctorsCount, string hospitalName, string city) : base(hospitalName, city)
+		{
+			this.WardName = wardName;
+			this.BedsCount = bedsCount;
+			this.DoctorsCount = doctorsCount;
+		}
+
+		public string WardName
+		{
+			get
+			{
+				return this.wardName;
+			}
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("The ward's name cannot be empty");
+				}
+				else
+				{
+					this.wardName = value;
+				}
+			}
+		}
+
+		public int BedsCount
+		{
+			get
+			{
+				return this.bedsCount;
+			}
+			set
+			{
+				if(value < 0)
+				{
+					throw new ArgumentException("The beds'count cannot be less than zero");
+				}
+				else
+				{
+					this.bedsCount = value;
+				}
+			}
+		}
+
+		public int DoctorsCount
+		{
+			get
+			{
+				return this.doctorsCount;
+			}
+			set
+			{
+				if (value < 0)
+				{
+					throw new ArgumentException("The doctors'count cannot be less than zero");
+				}
+				else
+				{
+					this.doctorsCount = value;
+				}
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{this.wardName} -> {this.Name} -> {this.City}{Environment.NewLine}" +
+				$"Брой легла - {this.bedsCount}; Брой работещи лекари - {this.doctorsCount}";
+		}
+	}
+}
