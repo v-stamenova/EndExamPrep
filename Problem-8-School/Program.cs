2e1d3212f6f19026c290b83ac2e7bd7b2fdada10
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-02-19 10:38:17 +0200

    Add solution to Problem 8

---
+++ Problem-8-School/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_8_School
+{
+	class Program
+	{
+		static List<Class> classes = new List<Class>();
+
+		static void Main(string[] args)
+		{
+		}
+
+		static void DisplayAllClasses() => classes.ForEach(x => Console.WriteLine(x));
+
+		static void DoesTheSchoolHaveFinancing(string school, int schoolYear)
+		{
+			Class searchedClass = classes.Find(x => x.Name == school && x.GraduationYear == schoolYear);
+
+			if(searchedClass.AverageFinalGrade > 5.5)
+			{
+				Console.WriteLine("Да - 10000лв");
+			}
+			else if(searchedClass.AverageFinalGrade > 5)
+			{
+				Console.WriteLine("Да - 8000лв");
+			}
+			else if(searchedClass.AverageFinalGrade > 4.5)
+			{
+				Console.WriteLine("Да - 5000лв");
+			}
+			else
+			{
+				Console.WriteLine("Не");
+			}
+		}
+
+		static void GetSchoolNamesAndStudentsGraduated(string city, int schoolYear)
+		{
+			List<Class> filteredClasses = classes.Where(x => x.City == city && x.GraduationYear == schoolYear).ToList();
+
+			filteredClasses.ForEach(x => Console.WriteLine($"{x.Name} - {x.StudentsCount} ученици"));
+			Console.WriteLine($"Общо: {filteredClasses.Sum(x => x.StudentsCount)} ученици");
+		}
+	}
+}
---
+++ Problem-8-School/School.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Problem_8_School
+{
+	class School
+	{
+		private string name;
+		private string city;
+
+		public School(string name, string city)
+		{
+			this.Name = name;
+			this.City = city;
+		}
+
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("The school's name cannot be empty");
+				}
+				else
+				{
+					this.name = value;
+				}
+			}
+		}
+
+		public string City
+		{
+			get
+			{
+				return this.city;
+			}
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("The city's name cannot be empty");
+				}
+				else
+				{
+					this.city = value;
+				}
+			}
+		}
+	}
+}
