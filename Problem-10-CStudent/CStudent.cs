b70036578d9a964f34eb104e055d8bc5e907b5d5
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-02-25 10:35:48 +0200

    Add solution to problem 11

---
+++ Problem-10-CStudent/CStudent.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_11_CStudent
+{
+	class CStudent
+	{
+		private string name;
+		private List<int> scores;
+		private string fn;
+
+		public CStudent(string name, string facultyNumber)
+		{
+			this.Name = name;
+			this.FacultyNumber = facultyNumber;
+			this.Scores = new List<int>();
+		}
+
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			private set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentNullException("The name should not be empty");
+				}
+				else
+				{
+					this.name = value;
+				}
+			}
+		}
+
+		public List<int> Scores
+		{
+			get
+			{
+				return this.scores;
+			}
+			private set
+			{
+				this.scores = value;
+			}
+		}
+
+		public string FacultyNumber
+		{
+			get
+			{
+				return this.fn;
+			}
+			private set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentNullException("The faculty number cannot be empty");
+				}
+				else
+				{
+					this.fn = value;
+				}
+			}
+		}
+
+		public double getGrade() => this.scores.Average();
+
+		public void addScore(int x)
+		{
+			if(x < 2 || x > 6)
+			{
+				throw new ArgumentException("The grade must be between 2 and 6");
+			}
+
+			this.scores.Add(x);
+		}
+	}
+}
