a013e844964aa75411d4fa54b0f935a480ce0d16
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-20 11:28:28 +0300

    Solve array problems

---
+++ Problem-2-Array/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem_2_Array
+{
+	public partial class Form1 : Form
+	{
+		int count = 0;
+		int[] array = new int[5];
+		
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if(count < array.Length)
+			{
+				int newElement = int.Parse(textBox1.Text);
+
+				if (array.Contains(newElement))
+				{
+					MessageBox.Show("Въведете число, което не се съдържа в масива");
+				}
+				else
+				{
+					array[count] = newElement;
+					textBox1.Clear();
+					count++;
+				}
+			}
+
+			if(count == array.Length)
+			{
+				button1.Enabled = false;
+				for(int i = 0; i < array.Length; i++)
+				{
+					listBox1.Items.Add($"{array[i]}");
+				}
+			}
+		}
+	}
+}
