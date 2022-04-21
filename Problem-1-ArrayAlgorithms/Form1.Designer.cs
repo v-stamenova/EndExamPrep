6b3f40ff0a1920d7f63a17a65dbe8863da83c637
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-21 12:13:24 +0300

    First problem

---
+++ Problem-1-ArrayAlgorithms/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Problem_1_ArrayAlgorithms
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(409, 91);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(206, 264);
+			this.listBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(124, 230);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(140, 60);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(124, 327);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(140, 60);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(61, 149);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(266, 26);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(728, 149);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(266, 26);
+			this.textBox2.TabIndex = 5;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(791, 230);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(140, 60);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1064, 480);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
