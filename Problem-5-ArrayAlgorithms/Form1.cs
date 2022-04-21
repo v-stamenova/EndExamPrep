103f9699881b85e927593c29914b462345015e29
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-21 18:15:13 +0300

    Rivers solution

---
+++ Problem-5-ArrayAlgorithms/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem_5_ArrayAlgorithms
+{
+	public partial class Form1 : Form
+	{
+		River[] rivers = new River[5];
+		int count = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if(count < rivers.Length)
+			{
+				River river = new River(textBox1.Text, double.Parse(textBox2.Text));
+				rivers[count] = river;
+
+				count++;
+			}
+
+			if(count == rivers.Length)
+			{
+				button1.Enabled = false;
+
+				for(int i = 0; i < rivers.Length; i++)
+				{
+					int minIndex = i;
+					for(int j = i; j < rivers.Length; j++)
+					{
+						if(rivers[minIndex].Compare(rivers[minIndex], rivers[j]) < 0)
+						{
+							minIndex = j;
+						}
+					}
+
+					River temp = rivers[minIndex];
+					rivers[minIndex] = rivers[i];
+					rivers[i] = temp;
+				}
+
+				for(int i = 0; i < rivers.Length; i++)
+				{
+					listBox1.Items.Add(rivers[i]);
+				}
+			}
+		}
+	}
+}
---
+++ Problem-5-ArrayAlgorithms/River.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_5_ArrayAlgorithms
+{
+	class River : IComparer
+	{
+		private string name;
+		private double length;
+
+		public River(string _name, double _length)
+		{
+			this.Name = _name;
+			this.Length = _length;
+		}
+
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentException("The name cannot be empty");
+				}
+				else
+				{
+					this.name = value;
+				}
+			}
+		}
+
+		public double Length
+		{
+			get
+			{
+				return this.length;
+			}
+			set
+			{
+				if(value <= 0)
+				{
+					throw new ArgumentException("Meters cannot be less than zero"); 
+				}
+				else
+				{
+					this.length = value;
+				}
+			}
+		}
+
+		public int Compare(object x, object y)
+		{
+			River first = (River)x;
+			River second = (River)y;
+
+			if(first.Length > second.Length)
+			{
+				return -1;
+			}
+			else if(first.Length < second.Length)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{this.name} - {this.length}";
+		}
+	}
+}
