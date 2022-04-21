0a298079849d1958d5132e4d27e1a738a2d8b590
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-21 10:32:06 +0300

    Solutions

---
+++ Problem-2-ADT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_2_ADT
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Queue<string> queue = new Queue<string>();
+			Stack<string> stack = new Stack<string>();
+
+			string word = Console.ReadLine();
+			while(word != "End")
+			{
+				queue.Enqueue(word);
+				stack.Push(word);
+
+				word = Console.ReadLine();
+			}
+
+			Console.WriteLine($"a) {string.Join("; ", queue)}");
+			Console.WriteLine($"b) {string.Join('-', stack)}");
+		}
+	}
+}
