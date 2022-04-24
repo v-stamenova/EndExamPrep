59aaff10da91ba4871a975b370ce3f2303bd90c1
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-24 14:56:24 +0300

    Solve

---
+++ Problem-3-DocumentOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_3_DocumentOne
+{
+	class Program
+	{
+		static List<Company> companies = new List<Company>();
+
+		static void Main(string[] args)
+		{
+			DataFill();
+
+			double width = double.Parse(Console.ReadLine());
+			double length = double.Parse(Console.ReadLine());
+			int cameraCount = int.Parse(Console.ReadLine());
+			string companyName = Console.ReadLine();
+
+			Company company = companies.Find(x => x.Name == companyName);
+			decimal totalPrice = (decimal)(width * length) * company.TypePrices[cameraCount];
+
+			Console.WriteLine($"Goshko has to spend {totalPrice.ToString("F2")} leva.");
+		}
+
+		static void DataFill()
+		{
+			companies.Add(new Company("Dogramichka4You", new Dictionary<int, decimal>() { {3, 12 }, {4, 15 }, {5, 20 } }));
+			companies.Add(new Company("TihoToplo", new Dictionary<int, decimal>() { {3, 15 }, {4, 14 }, {5, 18 } }));
+			companies.Add(new Company("ChukChuk", new Dictionary<int, decimal>() { {3, 14 }, {4, 20 }, {5, 22 } }));
+		}
+	}
+}
---
+++ Problem-4-DocumentOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_4_DocumentOne
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<double, string> earthquakes = new Dictionary<double, string>();
+			double amount = double.Parse(Console.ReadLine());
+
+			for(int i = 0; i < amount; i++)
+			{
+				double measure = double.Parse(Console.ReadLine());
+				earthquakes.Add(measure, DetermineType(measure));
+			}
+
+			Console.WriteLine($"Light: {((earthquakes.Where(x => x.Value == "Light").Count() / amount) * 100).ToString("F2")}%");
+			Console.WriteLine($"Moderate: {((earthquakes.Where(x => x.Value == "Moderate").Count() / amount) * 100).ToString("F2")}%");
+			Console.WriteLine($"Strong: {((earthquakes.Where(x => x.Value == "Strong").Count() / amount) * 100).ToString("F2")}%");
+			Console.WriteLine($"Very strong: {((earthquakes.Where(x => x.Value == "Very strong").Count() / amount) * 100).ToString("F2")}%");
+		}
+
+		static string DetermineType(double measure)
+		{
+			if(measure <= 4)
+			{
+				return "Light";
+			}
+			else if(measure <= 6)
+			{
+				return "Moderate";
+			}
+			else if(measure <= 7)
+			{
+				return "Strong";
+			}
+			else
+			{
+				return "Very strong";
+			}
+		}
+	}
+}
