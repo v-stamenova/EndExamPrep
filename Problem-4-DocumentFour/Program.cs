46a86e27072703762c2af0f6e75fe05f2a1bb5d2
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-25 22:45:01 +0300

    Solve all problems from document four

---
+++ Problem-4-DocumentFour/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_4_DocumentFour
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, int> peopleEggs = new Dictionary<string, int>();
+
+			List<string> input = Console.ReadLine().Split(' ').ToList();
+			while(input[0] != "Оut" && input[1] != "of")
+			{
+				if (peopleEggs.ContainsKey(input[0]))
+				{
+					peopleEggs[input[0]] += int.Parse(input[2]);
+				}
+				else
+				{
+					peopleEggs.Add(input[0], int.Parse(input[2]));
+				}
+
+				input = Console.ReadLine().Split(' ').ToList();
+			}
+
+			foreach(var pair in peopleEggs.OrderBy(x => x.Key))
+			{
+				Console.WriteLine($"{pair.Key} has beaten {pair.Value} eggs");
+			}
+		}
+	}
+}
