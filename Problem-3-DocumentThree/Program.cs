3e335d8639e572f4b2c986436b8b3f80f2c0bde4
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-25 19:44:22 +0300

    Solve the remaining problems

---
+++ Problem-3-DocumentThree/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_3_DocumentThree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, int> namesPoints = new Dictionary<string, int>();
+
+			string input = Console.ReadLine();
+			while(input != "END OF GAME")
+			{
+				namesPoints.Add(input, GetPoints(input));
+
+				input = Console.ReadLine();
+			}
+
+			KeyValuePair<string, int> winner = namesPoints.First(x => x.Value == namesPoints.Max(x => x.Value));
+			Console.WriteLine($"Winner is name: {winner.Key}");
+			Console.WriteLine($"Points: {winner.Value}");
+		}
+
+		static int GetPoints(string name)
+		{
+			int points = 0;
+
+			if (name.EndsWith('a'))
+			{
+				points += 10;
+			}
+			else if (name.EndsWith('v'))
+			{
+				points += 13;
+			}
+
+			if(name.Length > 6)
+			{
+				points += 33;
+			}
+			else
+			{
+				points += 22;
+			}
+
+			return points;
+		}
+	}
+}
---
+++ Problem-4-DocumentThree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_4_DocumentThree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, int> players = new Dictionary<string, int>();
+
+			List<string> input = Console.ReadLine().Split(' ').ToList();
+			while(input[0] != "End" && input[1] != "of")
+			{
+				if (players.ContainsKey(input[0]))
+				{
+					players[input[0]] += int.Parse(input[2]);
+				}
+				else
+				{
+					players.Add(input[0], int.Parse(input[2]));
+				}
+
+				input = Console.ReadLine().Split(' ').ToList();
+			}
+
+			foreach(var pair in players.OrderBy(x => x.Key))
+			{
+				Console.WriteLine($"{pair.Key} has passed {pair.Value} passes.");
+			}
+		}
+	}
+}
