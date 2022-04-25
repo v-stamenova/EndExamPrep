62b8c64c5218974f87602c75c89a9206bd3662ce
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-25 13:10:38 +0300

    Solve document 2

---
+++ Problem-1-DocumentTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem_1_DocumentTwo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int neons = int.Parse(Console.ReadLine());
+			int molensies = int.Parse(Console.ReadLine());
+			int helers = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(neons * 4 + helers * 8 + molensies * 8);
+		}
+	}
+}
---
+++ Problem-2-DocumentTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problem_2_DocumentTwo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			decimal budget = decimal.Parse(Console.ReadLine());
+			decimal bookPrice = decimal.Parse(Console.ReadLine());
+
+			if (budget - (bookPrice + bookPrice * (decimal)0.2 + bookPrice * (decimal)0.12 + bookPrice * (decimal)0.05) >= 0)
+			{
+				Console.WriteLine($"Yes! {(budget - (bookPrice + bookPrice * (decimal)0.2 + bookPrice * (decimal)0.12 + bookPrice * (decimal)0.05)).ToString("F2")} leva left.");
+			}
+			else
+			{
+				Console.WriteLine($"No! {((bookPrice + bookPrice * (decimal)0.2 + bookPrice * (decimal)0.12 + bookPrice * (decimal)0.05) - budget).ToString("F2")} leva needed.");
+			}
+		}
+	}
+}
